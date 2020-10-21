3e8c8840e2e0ced48187f30cd292f19dc945caed
Yingying Chen <cyy870119@gmail.com>
2020-10-21 15:23:51 -0400

    Initial Commit

---
+++ BuildNetflix/BuildNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixApp.swift
+//  BuildNetflix
+//
+//  Created by Yingying Chen on 10/21/20.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
